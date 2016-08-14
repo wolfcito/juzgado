e3da40410ea2bc8a267f43d5ab1ab7c1d2a939a9
cazaesclavas <flufenix@hotmail.com>
2016-08-13 22:08:02 -0500

    Actualización Base de datos Juzgado.

--- afectado/views.py
+++ afectado/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from .models import Afectado
 
 def lista_afectados(request):
-	return render(request, 'afectado/lista_afectados.html', {})
+	listado = Afectado.objects.order_by('cedula_afectado')
+	return render(request, 'afectado/lista_afectados.html', {'listado':listado})
 	
