fe54d13d194b9054abdf7d45614f055765bca4d6
cazaesclavas <flufenix@hotmail.com>
2016-08-13 20:29:03 -0500

    Actualización Web Difusa Juzgado

--- afectado/views.py
+++ afectado/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
 
-def home(request):
-	return render_to_response('afectado.html')
+def lista_afectados(request):
+	return render(request, 'afectado/lista_afectados.html', {})
+	
