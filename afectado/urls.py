c04e9ccd438f70d2adda118e9a89d1f913fca740
cazaesclavas <flufenix@hotmail.com>
2016-08-15 00:32:42 -0500

    Creación/Edición sección Afectado - BD Difusas - Juzgado

--- afectado/urls.py
+++ afectado/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import include, url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.lista_afectados),
+        url(r'^$', views.inicio),
+        url(r'^afectado/$', views.lista_afectados),
         url(r'^afectado/(?P<id>[0-9]+)/$', views.detalle_afectado),
+        url(r'^afectado/nuevo/$', views.nuevo_afectado, name='nuevo_afectado'),
+        url(r'^afectado/(?P<id>[0-9]+)/edit/$', views.editar_afectado, name='editar_afectado'),
 ]
--- afectado/views.py
+++ afectado/views.py
@@ -1,5 +1,10 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Afectado
+from .forms import FrmAfectado
+
+def inicio(request):
+	usuario = "Invitado"
+	return render(request, 'afectado/inicio.html',{'usuario':usuario})
 
 def lista_afectados(request):
 	listado = Afectado.objects.order_by('cedula_afectado')
@@ -8,3 +13,24 @@ def lista_afectados(request):
 def detalle_afectado(request,id):
 	detalle = get_object_or_404(Afectado, id_afectado=id)
 	return render(request, 'afectado/detalle_afectado.html',{'detalle':detalle})
+
+def nuevo_afectado(request):
+	if request.method == "POST":
+		form = FrmAfectado(request.POST)
+		if form.is_valid():
+			unAfectado = form.save()
+			return redirect('afectado.views.detalle_afectado',id=unAfectado.id_afectado)
+	else:
+		form = FrmAfectado()
+	return render(request,'afectado/nuevo_afectado.html',{'form':form})
+
+def editar_afectado(request,id):
+	edicion = get_object_or_404(Afectado, id_afectado = id)
+	if request.method == "POST":
+		form = FrmAfectado(request.POST, instance = edicion)
+		if form.is_valid():
+			edicion = form.save()
+			return redirect('afectado.views.detalle_afectado',id=edicion.id_afectado)
+	else:
+		form = FrmAfectado(instance=edicion)
+	return render(request,'afectado/nuevo_afectado.html',{'form':form})
